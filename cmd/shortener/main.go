5566e46a4d948984b88155bc6ba2fd0e5a7a6f28
SZabrodskii <141975567+SZabrodskii@users.noreply.github.com>
2026-05-27 17:20:28 -0300

    Revert "Iter1"

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,24 +1,3 @@
 package main
 
-import (
-	"github.com/SZabrodskii/url-shortener/internal/config"
-	"github.com/SZabrodskii/url-shortener/internal/handler"
-	"github.com/SZabrodskii/url-shortener/internal/repository"
-	"github.com/SZabrodskii/url-shortener/internal/server"
-	"github.com/SZabrodskii/url-shortener/internal/service"
-	"go.uber.org/fx"
-)
-
-func main() {
-	fx.New(createApp()).Run()
-}
-
-func createApp() fx.Option {
-	return fx.Options(
-		config.Provide(),
-		repository.Provide(),
-		service.Provide(),
-		handler.Provide(),
-		server.ProvideHTTPModule(),
-	)
-}
+func main() {}
--- internal/service/storage.go
+++
@@ -1,6 +0,0 @@
-package service
-
-type Storage interface {
-	Get(id string) (string, error)
-	Put(id, originalURL string) error
-}
